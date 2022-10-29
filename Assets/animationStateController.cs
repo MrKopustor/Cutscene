1e8ca5a7206b15f34ba77a8da4beb3673943303e
mr.rabukhin <zotop1753@yahoo.com>
2022-10-29 22:45:12 +0300

    second commit
    Setting up a player control

---
+++ Assets/animationStateController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationStateController : MonoBehaviour
+{
+    Animator animator; //Объявление ссылочной переменной 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if player presses w key
+        if (Input.GetKey("w"))
+        {
+            //then set the isWalking boolean to be true
+            animator.SetBool("isWalking", true);
+        }
+        // if player is not pressing w key 
+        if (!Input.GetKey("w")) 
+        {
+            //then set the isWalking boolean to be false 
+            animator.SetBool("isWlking", false);
+        }
+    }
+}
